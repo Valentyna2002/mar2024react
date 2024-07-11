c6020f688ef727a591a2cdce6d60a17bdd4af02d
macbook <valialevchuk1@gmail.com>
2024-07-11 08:58:42 -0500

    hw2

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import {IUser} from "../model/IUser";
+import {IPosts} from "../model/IPosts";
 
 let axiosInstance = axios.create({
     baseURL:'https://dummyjson.com'})
@@ -7,11 +9,11 @@ axiosInstance.interceptors.request.use(request =>{
     return request
 })
 
-const getAllUsers = async ()=>{
-    return await axiosInstance.get('/users').then((response) =>response.data)
+const getAllUsers = async ():Promise<IUser[]>=>{
+    return await axiosInstance.get<IUser[]>('/users').then((response) =>response.data)
 }
-const getPostsOfUserById = async (id:number)=>{
-    return await axiosInstance.get(`/users/${id}/posts/`).then((response) =>response.data)
+const getPostsOfUserById = async (id:number):Promise<IPosts[]>=>{
+    return await axiosInstance.get<IPosts[]>(`/users/${id}/posts/`).then((response) =>response.data)
 }
 export {getAllUsers, getPostsOfUserById}
 
